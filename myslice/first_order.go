046fcd3b74106a0908868d0402071b58e4c7c976
Matija Martinic <matija.martinic@gmail.com>
2024-08-12 14:03:13 +0200

    save & push

--- myslice/first_order.go
+++ myslice/first_order.go
@@ -7,3 +7,15 @@ func Map[T any, R any](slice []T, fnc func(T) R) []R {
 	}
 	return ret
 }
+
+func MapError[T any, R any](slice []T, fnc func(T) (R, error)) ([]R, error) {
+	ret := make([]R, 0, len(slice))
+	for _, el := range slice {
+		fncret, err := fnc(el)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, fncret)
+	}
+	return ret, nil
+}
